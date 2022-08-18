20df7a85e5781c9408445706f372ed9996836f44
Dewi Purba <56572846+dewi21@users.noreply.github.com>
2022-08-18 15:43:04 +0700

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,40 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'dewipurba03@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'dewipurba03@gmail.com',
+    'password' => 'Berjuang28',
+    'port' => '465'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 50);
+
+  echo $contact->send();
+?>
